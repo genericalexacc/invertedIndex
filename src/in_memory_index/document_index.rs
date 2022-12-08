6e6f31a44824b731cd75b7731e6e5f17105b029e
Alexander Shevchenko <shevchenkoalexander@icloud.com>
2022-12-07 21:54:00 -0800

    add okapi

--- src/in_memory_index/document_index.rs
+++ src/in_memory_index/document_index.rs
@@ -1,4 +1,4 @@
-use crate::in_memory_index::{DocId, IndexMap, NewDoc};
+use crate::in_memory_index::{DocId, DocStat, IndexMap, NewDoc, Stats};
 
 use anyhow::Result;
 use std::sync::{Arc, Mutex};
@@ -7,9 +7,14 @@ use std::thread::JoinHandle;
 // Interface for indexer.
 pub trait DocumentIndex {
     fn new() -> Self;
-    fn from_single_document(doc: NewDoc) -> IndexMap;
+    fn from_single_document(doc: NewDoc) -> (IndexMap, super::DocStat);
     fn add_single_document(&self, doc: NewDoc) -> JoinHandle<Result<()>>;
     fn add_multiple_documents(&self, docs: Vec<NewDoc>);
-    fn add_from_index_static(dict: Arc<Mutex<IndexMap>>, other: IndexMap) -> Result<()>;
+    fn add_from_index_static(
+        dict: (Arc<Mutex<IndexMap>>, Arc<Mutex<Stats>>),
+        other: (IndexMap, DocStat, NewDoc),
+    ) -> Result<()>;
     fn query(&self, query_text: String) -> Result<Vec<DocId>>;
+    fn query_okapi(&self, query_text: &str) -> Result<Vec<DocId>>;
+    fn bm25(&self, query: &str, document: &str) -> Result<f64>;
 }
